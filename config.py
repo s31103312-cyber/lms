f13ab62a992d7661c5f5cbfb8f2265256b9c10e5
s31103312-cyber <s31103312@gmail.com>
2026-05-25 18:45:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #database
-DATABASE_URL = "postgresql://postgres:NkfNZGCiVCnCTsFKDnBOBGsmVrqjvKxK@turntable.proxy.rlwy.net:45347/railway"
+DATABASE_URL = "postgresql://postgres:zDrJEBSAvPJLZmhjUjQIaKpUkfAKBzQv@shinkansen.proxy.rlwy.net:15945/railway"
 # === SMS PANEL CONFIG ===
 PANEL_BASE_URL = "http://51.210.208.26"
 PANEL_USERNAME = "NewGem"
